3226d4b4ce410c5c1daf8f1b844f491df1aae831
Eliot Reyes <eliotmij@gmail.com>
2016-07-29 19:56:17 -0700

    Added logic for displaying questions, capturing user feedback, and processing the feedback

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 # by Eliot Reyes
 # July 2016
 # UCLA Extension - The Coding Bootcamp
+
+Introduction
+-------------
+Trivia game made using JavaScript for the logic and jQuery to manipulate HTML.
+
+Game Logic
+
+Page loads
+Game begins when button is pressed
+First multiple-choice question is displayed along with a timer
+
+Possible results:
+1. User answers correctly
+2. User answers incorrectly
+3. Time runs out
+
+Each of the three above lead to a message and an image being displayed for a short amount of time. The message indicates wether or not the user answered correctly.
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,26 @@
 //Trivia Game
 //by Eliot Reyes
 //July 2016
+
+
+
+var knowledgeRepo = {
+  q1: {
+    prompt: "What is the name of Hagrid's hypogriff?",
+    correct: "a1",
+    ops: {
+      a1: "Buckbeak",
+      a2: "Fluffy",
+      a3: "Norbert",
+      a4: "Fang"
+    },
+    i: "assets/images/buckbeak.jpg"
+  }
+};
+
+var game = {};
+
+//Variable "gs" is used the jQuery selector for writing HTML output
 var gs = $('#gameSection');
 
 function initializeGame () {
@@ -18,11 +38,90 @@ function beginBtnHTML(){
 
 
 function startGame(){
-  gs.html("<h2>Hello World</h2>");
+  var gameTemplate = {
+    cq: 0, //Current Question
+    right: 0,
+    wrong: 0,
+    noAns: 0,
+    over: false,
+    qNum: ""
+  }
+
+  game = Object.assign({}, gameTemplate);
+
+  printQ();
+
+  btnListener();
+}
+
+function printQ(){
+  game.cq++ //Increment the value of the current question tracker
+
+  game.qNum = "q" + game.cq;
+
+  var q = $('<h3>');
+  q.html(knowledgeRepo[game.qNum].prompt);
+
+  gs.html(q);
+
+
+  for (k in knowledgeRepo[game.qNum].ops){
+    var o = $('<button>');
+    o.addClass('optionButton text-center');
+    o.attr('value', k);
+    o.html(knowledgeRepo[game.qNum].ops[k]);
+
+    gs.append(o);
+
+  }
+}
+
+function checkAns(b){
+  var ans = knowledgeRepo[game.qNum].correct;
+  console.log(ans);
+
+  var msg = "";
+  
+  if (b== knowledgeRepo[game.qNum].correct){
+    game.right++;
+    msg = "You answered correctly!";
+  }
+
+  else {
+    game.wrong++;
+    msg = "Your answer is incorrect! ";
+    msg += "The correct answer is " + knowledgeRepo[game.qNum].ops[ans];
+  }
+
+  result(msg);
+
+}
+
+function btnListener(){  
+  $('.optionButton').on('click', function(){
+    optionClicked = $(this).attr('value');
+    checkAns(optionClicked);
+  });
+}
+
+
+function result(m){
+  var p = $('<p>');
+  p.html(m);
+
+  gs.html(p);
+
+  var im = $('<img>');
+  im.addClass('resultImage');
+  im.attr('src', knowledgeRepo[game.qNum].i);
+
+  gs.append(im);
 }
 
 
 initializeGame();
 
-$('#begin').on('click', startGame);
+$(document).ready(function(){
+  $('#begin').on('click', startGame);
+});
 
